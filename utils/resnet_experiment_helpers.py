9e151fc9bbf231589488b3c94221fe6357b21d8e
Barbie <barbara.raven42@gmail.com>
2024-12-19 12:38:18 -0500

    modified training function to handle one hot encoded vectors

--- utils/resnet_experiment_helpers.py
+++ utils/resnet_experiment_helpers.py
@@ -300,9 +300,13 @@ def train_model(model, dataloaders, criterion, optimizer, scheduler, num_epochs=
 
                     # statistics
                     running_loss += loss.item() * inputs.size(0)
-                    print("labels.data.shape:", labels.data.shape)
-                    print("preds.shape:", preds.shape)
-                    running_corrects += torch.sum(preds == labels.data)
+                    # print("labels.data.shape:", labels.data.shape)
+                    # print("preds.shape:", preds.shape)
+                    label_indices = labels.data
+                    # if labels are one-hot-encoded, turn them back to indices
+                    if len(label_indices.shape) > 1:
+                        label_indices =  torch.argmax(labels, dim=1)
+                    running_corrects += torch.sum(preds == label_indices)
                     avg_class_acc, _ = calculate_per_class_accuracy(labels.data.cpu().numpy(), preds.cpu().numpy())
                     running_class_acc += avg_class_acc
                 if phase == 'train':
