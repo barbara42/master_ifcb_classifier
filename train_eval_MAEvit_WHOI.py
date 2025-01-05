128a31952ff2d520a0bc901ed1b1b0e7d340b836
Barbie <barbara.raven42@gmail.com>
2025-01-05 12:57:02 -0500

    mae vit from scratch

---
+++ train_eval_MAEvit_WHOI.py
@@ -0,0 +1,131 @@
+# imports
+import torchvision
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import models, transforms
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+import matplotlib.pyplot as plt
+import sys
+import math
+import os 
+from utils import resnet_experiment_helpers as helper
+from einops import repeat, rearrange
+
+from torch.utils.tensorboard import SummaryWriter
+
+from tqdm import tqdm
+
+# import the MAE code from local source 
+mae_src = "/home/birdy/meng_thesis/code/MAE"
+sys.path.append(mae_src)
+from model import MAE_ViT
+from model import ViT_Classifier
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+BATCH_SIZE = 256
+NUM_WORKERS = 2
+#NUM_EPOCHS = 31
+# LEARNING_RATE = 0.001
+mask_ratio = 0.75
+base_learning_rate = 1.5e-4
+weight_decay = 0.05
+warmup_epoch = 5
+total_epoch = 31
+
+dataset_name = "WHOI"
+model_name = f"MAEvit_scratch_{dataset_name}_{total_epoch}"
+DEST_ROOT = f"/nobackup/users/birdy/{model_name}-{dataset_name}"
+os.makedirs(DEST_ROOT, exist_ok=True)
+
+
+
+data_dir = f"/nobackup/projects/public/WHOI-Plankton/2014"
+csv_file = f"WHOI2014_labels.csv"
+train_dataset = helper.WHOIDataset(data_dir, csv_file, "train", transform=helper.data_transforms["train_basic"])
+val_dataset = helper.WHOIDataset(data_dir, csv_file, "val", transform=helper.data_transforms["val"])
+NUM_CLASSES = len(train_dataset.classes)
+
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
+val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
+
+dataloaders = {
+    'train': train_dataloader,
+    'val': val_dataloader
+}
+
+# logs stored in the writer
+log_dir = f"{DEST_ROOT}/logs/vit_cls_scratch"
+writer = SummaryWriter(log_dir)
+
+base_learning_rate = 1e-3
+weight_decay = 0.05
+warmup_epoch = 5
+total_epoch = 31
+
+model_name = f"vit-cls-{dataset_name}-from-scratch_{total_epoch}"
+vit_output_model_path = f"{DEST_ROOT}/{model_name}.pt"
+
+# load up trained MAE model 
+model = MAE_ViT()
+model = ViT_Classifier(model.encoder, num_classes=NUM_CLASSES).to(device)
+
+# set up optimizer, scheduler, loss function
+loss_fn = torch.nn.CrossEntropyLoss()
+acc_fn = lambda logit, label: torch.mean((logit.argmax(dim=-1) == label).float())
+optim = torch.optim.AdamW(model.parameters(), lr=base_learning_rate * BATCH_SIZE / 256, betas=(0.9, 0.999), weight_decay=weight_decay)
+lr_func = lambda epoch: min((epoch + 1) / (warmup_epoch + 1e-8), 0.5 * (math.cos(epoch / total_epoch * math.pi) + 1))
+lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optim, lr_lambda=lr_func, verbose=True)
+
+# TRAIN
+best_val_acc = 0
+optim.zero_grad()
+for e in range(total_epoch):
+    model.train()
+    losses = []
+    acces = []
+    for img, label in tqdm(iter(train_dataloader)):
+        img = img.to(device)
+        label = label.to(device)
+        logits = model(img)
+        loss = loss_fn(logits, label)
+        acc = acc_fn(logits, label)
+        loss.backward()
+
+        optim.step()
+        optim.zero_grad()
+        losses.append(loss.item())
+        acces.append(acc.item())
+    lr_scheduler.step()
+    avg_train_loss = sum(losses) / len(losses)
+    avg_train_acc = sum(acces) / len(acces)
+    print(f'In epoch {e}, average training loss is {avg_train_loss}, average training acc is {avg_train_acc}.')
+
+    model.eval()
+    with torch.no_grad():
+        losses = []
+        acces = []
+        for img, label in tqdm(iter(val_dataloader)):
+            img = img.to(device)
+            label = label.to(device)
+            logits = model(img)
+            loss = loss_fn(logits, label)
+            acc = acc_fn(logits, label)
+            losses.append(loss.item())
+            acces.append(acc.item())
+        avg_val_loss = sum(losses) / len(losses)
+        avg_val_acc = sum(acces) / len(acces)
+        print(f'In epoch {e}, average validation loss is {avg_val_loss}, average validation acc is {avg_val_acc}.')  
+
+    if avg_val_acc > best_val_acc:
+        best_val_acc = avg_val_acc
+        print(f'saving best model with acc {best_val_acc} at {e} epoch!')       
+        torch.save(model, vit_output_model_path)
+
+    writer.add_scalars('cls/loss', {'train' : avg_train_loss, 'val' : avg_val_loss}, global_step=e)
+    writer.add_scalars('cls/acc', {'train' : avg_train_acc, 'val' : avg_val_acc}, global_step=e)
